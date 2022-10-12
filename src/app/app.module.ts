4f075fba3089928c9e42ec2fa626789ba16b726a
Jithin Ramesh <109571606+jithinramesh@users.noreply.github.com>
2022-10-12 13:58:05 +0530

    hf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { TodoComponent } from './todo/todo.component';
+import{HttpClientModule}from '@angular/common/http'
 
 const link:Routes=[
   {path:"",component:TodoComponent}
@@ -20,6 +21,7 @@ const link:Routes=[
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot(link)
   ],
   providers: [],
